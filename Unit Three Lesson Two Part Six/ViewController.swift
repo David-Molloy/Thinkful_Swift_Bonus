27c19f5bcfbc08f21b67413f21a1d6dd49598058
David-Molloy <dmolloy@riis.com>
2015-03-24 12:35:34 +0000

    Initial Commit

---
+++ Unit Three Lesson Two Part Six/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit Three Lesson Two Part Six
+//
+//  Created by David Molloy on 24/03/2015.
+//  Copyright (c) 2015 riis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
